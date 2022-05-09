39b972f9d510f713c8fa265c65d62cc264b7410c
Justin Apupalo <69662609+J-a-crypto@users.noreply.github.com>
2022-05-09 18:15:27 -0400

    Day2 Changes A decent amount of progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
+import { useState } from "react";
 import "./styles.css";
+import React from "react";
 
 export default function App() {
+  const [playerTurn, changePlayerTurn] = useState("red");
+  const [tokens, changeTokens] = useState([]);
+  //Note: Use an array as the intial value to keep track of the tokens dropped
+
+  function togglePlayerTurn() {
+    if (playerTurn === "red") {
+      changePlayerTurn("yellow");
+    } else if (playerTurn === "yellow") {
+      changePlayerTurn("red");
+    }
+  }
+
+  function addToken() {
+    changeTokens([...tokens, playerTurn]);
+    togglePlayerTurn();
+  }
+
   return (
     <div className="App">
-      <h1>Hello My React App</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div class="board" onClick={() => addToken()}>
+        <div class="BlueSquare">
+          <button class={`${tokens[4]} circle`}></button>
+        </div>
+        <div class="BlueSquare">
+          <button class={`${tokens[3]} circle`}></button>
+        </div>
+        <div class="BlueSquare">
+          <button class={`${tokens[2]} circle`}></button>
+        </div>
+        <div class="BlueSquare">
+          <button class={`${tokens[1]} circle`}></button>
+        </div>
+        <div class="BlueSquare">
+          <button class={`${tokens[0]} circle`}></button>
+        </div>
+        <p>It is {playerTurn}'s turn</p>
+      </div>
     </div>
   );
 }
