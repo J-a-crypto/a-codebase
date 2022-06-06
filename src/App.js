f3a783d6ea2b5717769dfbe8fb158cd118c856b3
Justin Apupalo <69662609+J-a-crypto@users.noreply.github.com>
2022-06-06 17:32:01 -0400

    Final Day

--- src/App.js
+++ src/App.js
@@ -9,42 +9,34 @@ export default function App() {
 
   //Note: Use an array as the intial value to keep track of the tokens dropped
   function playColumn(columnNumber) {
-    updateBoard([...board[columnNumber], playerTurn]);
+    updateBoard([...board, playerTurn]);
   }
   function togglePlayerTurn() {
-    if (playerTurn === "red") {
-      changePlayerTurn("yellow");
-    } else if (playerTurn === "yellow") {
-      changePlayerTurn("red");
+    if (tokens.length < 6) {
+      if (playerTurn === "red") {
+        changePlayerTurn("yellow");
+      } else if (playerTurn === "yellow") {
+        changePlayerTurn("red");
+      }
     }
   }
 
   function addToken() {
     changeTokens([...tokens, playerTurn]);
     togglePlayerTurn();
+    playColumn();
   }
 
   function Column(props) {
     return (
       <div class="column" onClick={() => props.onClick()}>
-        <div class="BlueSquare">
-          <button class={`${props.tokens[5]} circle`}></button>
-        </div>
-        <div class="BlueSquare">
-          <button class={`${props.tokens[4]} circle`}></button>
-        </div>
-        <div class="BlueSquare">
-          <button class={`${props.tokens[3]} circle`}></button>
-        </div>
-        <div class="BlueSquare">
-          <button class={`${props.tokens[2]} circle`}></button>
-        </div>
-        <div class="BlueSquare">
-          <button class={`${props.tokens[1]} circle`}></button>
-        </div>
-        <div class="BlueSquare">
-          <button class={`${props.tokens[0]} circle`}></button>
-        </div>
+        {board.map((a) => (
+          <div>
+            <div class="BlueSquare">
+              <button class={`${props.tokens[a]} circle`}></button>
+            </div>
+          </div>
+        ))}
       </div>
     );
   }
@@ -52,8 +44,12 @@ export default function App() {
   return (
     <div className="App">
       <div class="board">
-        <Column tokens={board[0]} onClick={() => addToken()} />;
-        <Column tokens={board[1]} onClick={() => addToken()} />;
+        <Column tokens={board[0]} onClick={() => addToken()} />,
+        <Column tokens={board[1]} onClick={() => addToken()} />,
+        <Column tokens={board[2]} onClick={() => addToken()} />,
+        <Column tokens={board[3]} onClick={() => addToken()} />,
+        <Column tokens={board[4]} onClick={() => addToken()} />,
+        <Column tokens={board[5]} onClick={() => addToken()} />,
         <p>It is {playerTurn}'s turn</p>
       </div>
     </div>
